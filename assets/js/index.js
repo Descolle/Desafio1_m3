60bff38690c40ba9b96e212ae9485bdbbcf0e48c
Tomas <tomascorvalan14@gmail.com>
2024-06-08 19:39:43 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+let precio = 400000;
+let cantidad = 0;
+let totalPagar = 0;
+let precioSpan = document.querySelector(".precio-inicial");
+let document.querySelector('.cantidad');
+precioSpan.innerHTML = precio;
+
+let pagarTotal = document.querySelector(".valor-total");
+let resultado = document.querySelector(".cantidad");
+let total = Number(resultado*precioSpan);
+total.innerHTML = pagarTotal
