52274fe8b030ca4c9c0264fe4b5113e1d89aea07
Tomas <tomascorvalan14@gmail.com>
2024-06-08 20:26:20 -0400

    multiplica a mayor

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,4 @@
-let precio = 400000;
-let cantidad = 0;
-let totalPagar = 0;
+let precio = 400000
+let cantidad = 0
 let precioSpan = document.querySelector(".precio-inicial");
-let document.querySelector('.cantidad');
-precioSpan.innerHTML = precio;
-
-let pagarTotal = document.querySelector(".valor-total");
-let resultado = document.querySelector(".cantidad");
-let total = Number(resultado*precioSpan);
-total.innerHTML = pagarTotal
+precioSpan.innerHTML = precio
